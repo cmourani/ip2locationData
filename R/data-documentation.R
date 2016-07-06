9d4e5c609bfd3549dbbfc8b9ebd42d588d9b18c3
dethfromabove <mejiajon@gmail.com>
2016-07-06 09:10:39 -0700

    deleted large data file, added program files

---
+++ R/data-documentation.R
@@ -0,0 +1,31 @@
+#' IP database - large
+#'
+#' A dataset containing 3,832,909 IP integer ranges and their corresponding country
+#' names, abbreviations, regions, cities, latitudes, longitudes, zip codes, and Greenwich Mean Times
+#'
+#' Last updated: 06/02/2016
+#'
+#'
+#' @format A data.frame with four variables
+#' \describe{
+#'   \item{IPfrom}{Start of IP integer range}
+#'   \item{IPto}{End of IP integer range}
+#'   \item{Abrv}{Two-character country code based on ISO 3166.}
+#'   \item{Country}{Country name based on ISO 3166.}
+#'   \item{Region}{Region within country}
+#'   \item{City}{City within region}
+#'   \item{Lat}{Latitude}
+#'   \item{Long}{Longitude}
+#'   \item{Zip}{Zip code}
+#'   \item{GMT}{Greenwich Mean Time}
+#' }
+#' @source \url{http://lite.ip2location.com}
+"ip2location.lite.db11"
+
+utils::globalVariables(c("IPfrom", "IPto",
+                         "Country", ".", ":=", "i", "Abrv",
+                         "City", "GMT", "Lat", "Long", "Region",
+                         "Zip", "aggregate", "group", "ip2location.lite.db11",
+                         "lat", "long", "prop"
+)
+)
---
+++ README.md
@@ -0,0 +1,22 @@
+# ip2location_data
+
+[![Build Status](https://travis-ci.org/gitronald/ip2location_data.svg?branch=master)](https://travis-ci.org/gitronald/ip2location_data)
+
+<!--
+[![CRAN_Downloads_Badge](http://cranlogs.r-pkg.org/badges/grand-total/IPtoCountryDB11)](http://cran.r-project.org/package=ip2location_data)
+[![CRAN_Status_Badge](http://www.r-pkg.org/badges/version/ip2location_data)](http://cran.r-project.org/package=ip2location_data)
+-->
+
+
+* A database of IP address and corresponding location details; used with package "IPtoCountry"
+
+<sub>Default database is available by Creative Commons Attribution-ShareAlike 4.0 Interational license (CC-BY-SA 4.0).  
+This site or product includes IP2Location LITE data available from http://www.ip2location.com.</sub>
+
+### Getting Started
+``` {r}
+devtools::install_github("gitronald/ip2location_data")
+library(ip2location_data)
+data(ip2location.lite.db11)
+```
+
